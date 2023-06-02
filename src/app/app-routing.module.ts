10e6fc816a87d9267cb20ccdd8d7da1043019e84
DseCasis <dse.casis@yavirac.edu.ec>
2023-06-01 22:06:38 -0500

    creacion del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { AuthRoutingModule } from './auth/auth-routing.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoPagesFoundComponent } from './no-pages-found/no-pages-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'/login', pathMatch: 'full' },
+  { path: '**', component: NoPagesFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AuthRoutingModule,
+  ],
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { Routes } from '@angular/router';
+import { NoPagesFoundComponent } from './no-pages-found/no-pages-found.component';
+
+const routes: Routes = [
+  { path : '', component: LoginComponent},
+  { path : 'login', component: LoginComponent},
+  // { path : 'login', component: LoginComponent, canActivate: [AuthGuard]
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoPagesFoundComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
